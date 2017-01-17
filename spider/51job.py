bed816764f22876f6ec83f188bbdb1a2c2b81ea2
dwz <1015479445@qq.com>
2017-01-17 14:16:05 +0800

    update 51job

--- spider/51job.py
+++ spider/51job.py
@@ -20,11 +20,11 @@ class Job_qcwy(threading.Thread):
         threading.Thread.__init__(self)
         # self.url = 'http://search.51job.com/jobsearch/search_result.php?fromJs=1&jobarea='+jobarea+'&keyword='+keyword+'&keywordtype=2&curr_page='+str(pn)+'&lang=c&stype=2&postchannel=0000&fromType=1&confirmdate=9'
         self.city = jobarea
-        self.keywords = ['python', '大数据', '']
+        self.keywords = ['python', '大数据']
         self.headers = {
             'Host': 'search.51job.com',
             'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/537.75.14',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
             }
         self.filepath = '/Users/edz/Documents/project/PositionAnalysis/positions.csv'
@@ -57,36 +57,45 @@ class Job_qcwy(threading.Thread):
 
 
     def run(self):
-        # self.conn()
+        self.conn()
         f = open(self.filepath, 'a')
         w = csv.writer(f)
         try:
-            # with self.db.cursor() as cur:
-            for i in range(len(self.keywords)):
-                for j in range(1,2):
-                    self.url = 'http://search.51job.com/jobsearch/search_result.php?fromJs=1&jobarea='+self.city+'&keyword='+self.keywords[i]+\
-                               '&keywordtype=2&curr_page='+str(j)+'&lang=c&stype=2&postchannel=0000&fromType=1&confirmdate=9'
-                    # html = download(self.url, self.headers)
-                    html = self.session.get(self.url, headers=self.headers)
-                    html.encoding = 'gbk'
-                    bsobj = BeautifulSoup(html.text, 'html.parser')
-                    table = bsobj.find('div', {'class', 'dw_table'})
-                    all_els = table.findAll('div', {'class', 'el'})
-                    for i in range(1, len(all_els)):
-                        today = all_els[i].find('span', {'class', 't5'}).get_text()
-                        if today == self.today:     # 判断发布的职位是否是当天的
-                            link = all_els[i].find('a')['href']
-                            position, salary, area, Description_and_requirements, companyName, companyNature, companyPersonnel, companyIntroduction = self.get_info(link)
-                            positionId = re.findall(r'\d+', link)[1]
-                            publish_time = self.today
-                            print position, companyName
-                            w.writerow([positionId, position, salary, Description_and_requirements, area, publish_time, companyName, companyNature, companyPersonnel, companyIntroduction])
-                                # sql = "INSERT INTO `` () VALUE ()"
-                                # cur.execute(sql)
-                                # self.db.commit()
+            with self.db.cursor() as cur:
+                for i in range(len(self.keywords)):
+                    for j in range(1,2):
+                        self.url = 'http://search.51job.com/jobsearch/search_result.php?fromJs=1&jobarea='+self.city+'&keyword='+self.keywords[i]+\
+                                   '&keywordtype=2&curr_page='+str(j)+'&lang=c&stype=2&postchannel=0000&fromType=1&confirmdate=9'
+                        html = download(self.url, self.headers)
+                        # html = self.session.get(self.url, headers=self.headers)
+                        html.encoding = 'gbk'
+                        bsobj = BeautifulSoup(html.text, 'html.parser')
+                        table = bsobj.find('div', {'class', 'dw_table'})
+                        all_els = table.findAll('div', {'class', 'el'})
+                        for i in range(1, len(all_els)):
+                            today = all_els[i].find('span', {'class', 't5'}).get_text()
+                            if today == self.today:     # 判断发布的职位是否是当天的
+                                link = all_els[i].find('a')['href']
+                                positionId = re.findall(r'\d+', link)[1]
+                                s = "SELECT `Position` FROM `51job_position_info` where `PositionId`='" + positionId + "'"
+                                if cur.execute(s) == 0:
+                                    position, salary, area, Description_and_requirements, companyName, companyNature, companyPersonnel, companyIntroduction = self.get_info(link)
+                                    publish_time = self.today
+                                    print position, companyName
+                                    w.writerow([positionId, position, salary, Description_and_requirements, area, publish_time, companyName, companyNature, companyPersonnel, companyIntroduction])
+                                    try:
+                                        sql = "INSERT INTO `51job_position_info` (`PositionId`, `Position`, `Salary`, `Description`, `Address`, `Publish_time`, `Company`, `CompanyNature`, `CompanyPersonnel`, `CompanyIntroduction` ) " \
+                                              "VALUE ('"+positionId+"','"+position+"','"+salary+"','"+Description_and_requirements+"','"+area+"','"+publish_time+"','"+companyName+"','"+companyNature+"','"+companyPersonnel+"','"+companyIntroduction+"')"
+                                        cur.execute(sql)
+                                        self.db.commit()
+                                        print sql
+                                    except Exception as e:
+                                        print e.message
+                                else:
+                                    print "已存在"
         finally:
             f.close()
-            # self.db.close()
+            self.db.close()
 
     # 数据库设置
     def conn(self):
@@ -103,8 +112,8 @@ class Job_qcwy(threading.Thread):
     def get_info(self, link):
         try:
             self.headers['Host'] = 'jobs.51job.com'
-            # html = download(link, self.headers)
-            html = self.session.get(link, headers=self.headers)
+            html = download(link, self.headers)
+            # html = self.session.get(link, headers=self.headers)
             html.encoding = 'gbk'
             bs = BeautifulSoup(html.text, 'html.parser')
 
@@ -116,31 +125,49 @@ class Job_qcwy(threading.Thread):
             lname = cn.find('span').get_text()
             # 薪资
             salary = cn.find('strong').get_text()
-            infos = bs.find('div', {'class', 'tCompany_main'}).find('div', {'class', 'bmsg job_msg inbox'}).get_text().replace('\t', '')
+            if salary == None:
+                salary = ''
 
-            sps = bs.findAll('span', {'class', 'sp4'})
-            # 经验要求
-            experience = sps[0].get_text().replace('\n', '')
-            # 学历要求
-            education = sps[1].get_text().replace('\n', '')
+            infos = bs.find('div', {'class', 'tCompany_main'}).find('div', {'class', 'bmsg job_msg inbox'}).get_text().replace('\t', '').replace('\r', '')
+            # sps = bs.findAll('span', {'class', 'sp4'})
+            # # 经验要求
+            # experience = sps[0].get_text().replace('\n', '')
+            # # 学历要求
+            # education = sps[1].get_text().replace('\n', '')
 
-            #职位描述和任职要求
-            Description_and_requirements = re.findall(r'(.*?)\r\n', infos)[0] + re.findall(r'(.*?)\r\n', infos)[1]
+            if infos != None:
+                if re.findall(r'(.*?)\n', infos)[0] == '':
+                #职位描述和任职要求
+                    Description_and_requirements = re.findall(r'(.*?)\n', infos)[1] + re.findall(r'(.*?)\n', infos)[2]
+                else:
+                    Description_and_requirements = re.findall(r'(.*?)\n', infos)[0] + re.findall(r'(.*?)\n', infos)[1]
+            else:
+                Description_and_requirements = ''
 
             a = bs.find('div', {'class', 'bmsg inbox'}).find('p', {'class', 'fp'})
+            if a == None:
+                a = ''
             # 工作地点
             area = lname + '-' + re.findall(r'span>(.*?)<', str(a))[0].replace('\t', '')
 
             #公司
             companyName = cn.find('p', {'class', 'cname'}).find('a').get_text()
+            if companyName == None:
+                companyName = ''
             company = cn.find('p', {'class', 'msg ltype'}).get_text().replace('\r', '').replace('\t', '').replace(' ', '')
             company = company.decode('utf-8').encode('utf-8')
             # 公司性质
             companyNature = re.findall(r'(.*?)\xc2\xa0', company)[0]
+            if companyNature == None:
+                companyNature = ''
             # 公司规模
             companyPersonnel = re.findall(r'(.*?)\xc2\xa0', company)[4]
+            if companyPersonnel == None:
+                companyPersonnel = ''
             #公司描述
             companyIntroduction = bs.find('div', {'class', 'tmsg inbox'}).get_text().replace('\t', '').replace('\n', '')
+            if companyIntroduction == None:
+                companyIntroduction = ''
 
             return JobName, salary, area, Description_and_requirements, companyName, companyNature, companyPersonnel, companyIntroduction
 
@@ -153,7 +180,7 @@ if __name__ == '__main__':
     if not os.path.isfile(filepath):
         f = open(filepath, 'a')
         writer = csv.writer(f)
-        writer.writerow(['positionId', 'position', 'salary', 'Description_and_requirements', 'area', 'publish_time','companyName', 'companyNature', 'companyPersonnel', 'companyIntroduction'])
+        writer.writerow(['positionId', 'position', 'salary', 'experience', 'education', 'Description_and_requirements', 'area', 'publish_time','companyName', 'companyNature', 'companyPersonnel', 'companyIntroduction'])
         f.close()
     jobarea = ['010000','020000']
 
