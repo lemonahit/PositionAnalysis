21f4c96d25d13be8ec69c5a77ec1f61e7d7d6b0a
dwz <1015479445@qq.com>
2017-02-08 15:08:53 +0800

    update 51job

--- spider/51job.py
+++ spider/51job.py
@@ -20,7 +20,7 @@ class Job_qcwy(threading.Thread):
         threading.Thread.__init__(self)
         # self.url = 'http://search.51job.com/jobsearch/search_result.php?fromJs=1&jobarea='+jobarea+'&keyword='+keyword+'&keywordtype=2&curr_page='+str(pn)+'&lang=c&stype=2&postchannel=0000&fromType=1&confirmdate=9'
         self.city = jobarea
-        self.keywords = ['python', '大数据']
+        self.keywords = ['大数据']
         self.headers = {
             'Host': 'search.51job.com',
             'Upgrade-Insecure-Requests': '1',
@@ -72,28 +72,32 @@ class Job_qcwy(threading.Thread):
                         bsobj = BeautifulSoup(html.text, 'html.parser')
                         table = bsobj.find('div', {'class', 'dw_table'})
                         all_els = table.findAll('div', {'class', 'el'})
-                        print len(all_els)
-                        for i in range(1, len(all_els)):
-                            today = all_els[i].find('span', {'class', 't5'}).get_text()
-                            if today == self.today:     # 判断发布的职位是否是当天的
-                                link = all_els[i].find('a')['href']
-                                positionId = re.findall(r'\d+', link)[1]
-                                s = "SELECT `Position` FROM `51job_position_info` where `PositionId`='" + positionId + "'"
-                                if cur.execute(s) == 0:
-                                    position, salary, area, Description_and_requirements, companyName, companyNature, companyPersonnel, companyIntroduction = self.get_info(link)
-                                    publish_time = self.today
-                                    print position, salary, area, companyName
-                                    # w.writerow([positionId, position, salary, Description_and_requirements, area, publish_time, companyName, companyNature, companyPersonnel, companyIntroduction])
-                                    try:
-                                        sql = "INSERT INTO `51job_position_info` (`PositionId`, `Position`, `Salary`, `Description`, `Address`, `Publish_time`, `Company`, `CompanyNature`, `CompanyPersonnel`, `CompanyIntroduction` ) " \
-                                              "VALUE ('"+positionId+"','"+position+"','"+salary+"','"+Description_and_requirements+"','"+area+"','"+publish_time+"','"+companyName+"','"+companyNature+"','"+companyPersonnel+"','"+companyIntroduction+"')"
-                                        cur.execute(sql)
-                                        self.db.commit()
-                                        print sql
-                                    except Exception as e:
-                                        print e
-                                else:
-                                    print positionId + "已存在"
+                        if all_els:
+                            print len(all_els)
+                            for i in range(1, len(all_els)):
+                                today = all_els[i].find('span', {'class', 't5'}).get_text()
+                                if today == self.today:     # 判断发布的职位是否是当天的
+                                    link = all_els[i].find('a')['href']
+                                    print link
+                                    positionId = re.findall(r'\d+', link)[1]
+                                    s = "SELECT `Position` FROM `51job_position_info` where `PositionId`='" + positionId + "'"
+                                    if cur.execute(s) == 0:
+                                        position, salary, area, Description_and_requirements, companyName, companyNature, companyPersonnel, companyIntroduction = self.get_info(link)
+                                        publish_time = self.today
+                                        print position, salary, area, companyName
+                                        # w.writerow([positionId, position, salary, Description_and_requirements, area, publish_time, companyName, companyNature, companyPersonnel, companyIntroduction])
+                                        try:
+                                            sql = "INSERT INTO `51job_position_info` (`PositionId`, `Position`, `Salary`, `Description`, `Address`, `Publish_time`, `Company`, `CompanyNature`, `CompanyPersonnel`, `CompanyIntroduction` ) " \
+                                                  "VALUE ('"+positionId+"','"+position+"','"+salary+"','"+Description_and_requirements+"','"+area+"','"+publish_time+"','"+companyName+"','"+companyNature+"','"+companyPersonnel+"','"+companyIntroduction+"')"
+                                            cur.execute(sql)
+                                            self.db.commit()
+                                            print sql
+                                        except Exception as e:
+                                            print e
+                                    else:
+                                        print positionId + "已存在"
+                        else:
+                            print all_els
         finally:
             f.close()
             self.db.close()
@@ -124,6 +128,8 @@ class Job_qcwy(threading.Thread):
             # 职位
             JobName = cn.find('h1').get_text()
             lname = cn.find('span').get_text()
+            if lname == None:
+                lname = ''
             # 薪资
             salary = cn.find('strong').get_text()
             if salary == None:
@@ -138,9 +144,15 @@ class Job_qcwy(threading.Thread):
             if infos != '':
                 if re.findall(r'(.*?)\n', infos)[0] == '':
                 #职位描述和任职要求
-                    Description_and_requirements = re.findall(r'(.*?)\n', infos)[1] + re.findall(r'(.*?)\n', infos)[2]
+                    try:
+                        Description_and_requirements = re.findall(r'(.*?)\n', infos)[1] + re.findall(r'(.*?)\n', infos)[2]
+                    except:
+                        Description_and_requirements = ''
                 else:
-                    Description_and_requirements = re.findall(r'(.*?)\n', infos)[0] + re.findall(r'(.*?)\n', infos)[1]
+                    try:
+                        Description_and_requirements = re.findall(r'(.*?)\n', infos)[0] + re.findall(r'(.*?)\n', infos)[1]
+                    except:
+                        Description_and_requirements = ''
             else:
                 Description_and_requirements = ''
 
@@ -161,34 +173,45 @@ class Job_qcwy(threading.Thread):
                 company = company.decode('utf-8').encode('utf-8')
             except:
                 pass
-            if re.findall(r'(.*?)\xc2\xa0', company) != []:
-                q = re.findall(r'(.*?)\xc2\xa0', company)[0]
-                if re.findall(r'\d', q) == []:
-                    # 公司性质
-                    companyNature = q
-                    if len(re.findall(r'(.*?)\xc2\xa0', company)) > 2:
-                        # 公司规模
-                        companyPersonnel = re.findall(r'(.*?)\xc2\xa0', company)[4]
+            if company:
+                if re.findall(r'(.*?)\xc2\xa0', company) != []:
+                    q = re.findall(r'(.*?)\xc2\xa0', company)[0]
+                    if re.findall(r'\d', q) == []:
+                        # 公司性质
+                        companyNature = q
+                        if len(re.findall(r'(.*?)\xc2\xa0', company)) > 2:
+                            # 公司规模
+                            try:
+                                companyPersonnel = re.findall(r'(.*?)\xc2\xa0', company)[4]
+                            except:
+                                companyPersonnel = ''
+                        else:
+                            companyPersonnel = ''
                     else:
-                        companyPersonnel = ''
+                        companyNature = ''
+                        companyPersonnel = q
                 else:
-                    companyNature = ''
-                    companyPersonnel = q
+                    q = re.findall(r'(.*?)\xa0\xa0', company)[0]
+                    if re.findall(r'\d', q) == []:
+                        companyNature = q
+                        if len(re.findall(r'(.*?)\xa0\xa0', company)) > 2:
+                            try:
+                                companyPersonnel = re.findall(r'(.*?)\xa0\xa0', company)[2]
+                            except:
+                                companyPersonnel = ''
+                        else:
+                            companyPersonnel = ''
+                    else:
+                        companyNature = ''
+                        companyPersonnel = q
             else:
-                q = re.findall(r'(.*?)\xa0\xa0', company)[0]
-                if re.findall(r'\d', q) == []:
-                    companyNature = q
-                    if len(re.findall(r'(.*?)\xa0\xa0', company)) > 2:
-                        companyPersonnel = re.findall(r'(.*?)\xa0\xa0', company)[2]
-                    else:
-                        companyPersonnel = ''
-                else:
-                    companyNature = ''
-                    companyPersonnel = q
+                companyNature = ''
+                companyPersonnel = ''
 
             #公司描述
-            companyIntroduction = bs.find('div', {'class', 'tmsg inbox'}).get_text().replace('\t', '').replace('\n', '')
-            if companyIntroduction == None:
+            try:
+                companyIntroduction = bs.find('div', {'class', 'tmsg inbox'}).get_text().replace('\t', '').replace('\n', '')
+            except:
                 companyIntroduction = ''
 
             return JobName, salary, area, Description_and_requirements, companyName, companyNature, companyPersonnel, companyIntroduction
@@ -205,7 +228,7 @@ if __name__ == '__main__':
     #     writer = csv.writer(f)
     #     writer.writerow(['positionId', 'position', 'salary', 'Description_and_requirements', 'area', 'publish_time','companyName', 'companyNature', 'companyPersonnel', 'companyIntroduction'])
     #     f.close()
-    jobarea = ['010000','020000']
+    jobarea = ['010000']
 
     threads = []
 
